6e825878a174704fbe74955fd1a91060e5b7904e
Attogram Project <attogram@users.noreply.github.com>
2017-10-23 21:27:24 +0200

    -c

--- docs/examples/search.categories.php
+++ docs/examples/search.categories.php
@@ -10,31 +10,3 @@ $results = $category->search('Albert Einstein');
 foreach ($results as $result) {
     print_r($result);
 }
-
-/* RESULT:
-
-Array
-(
-    [pageid] => 970886
-    [ns] => 14
-    [title] => Category:Albert Einstein
-    [index] => 1
-    [categoryinfo.size] => 198
-    [categoryinfo.pages] => 3
-    [categoryinfo.files] => 177
-    [categoryinfo.subcats] => 18
-    [categoryinfo.hidden] => 
-)
-Array
-(
-    [pageid] => 41480939
-    [ns] => 14
-    [title] => Category:Albert-Einstein-Schule Laatzen
-    [index] => 2
-    [categoryinfo.size] => 1
-    [categoryinfo.pages] => 0
-    [categoryinfo.files] => 1
-    [categoryinfo.subcats] => 0
-    [categoryinfo.hidden] => 
-)
-*/
